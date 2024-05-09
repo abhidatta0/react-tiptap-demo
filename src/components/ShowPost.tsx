4442216dbbf6c028fc7e71c828dad57e2f220fe9
abhidatta0 <abhidatta146@gmail.com>
2024-05-09 16:35:47 +0530

    fix: tailwind oveeride issue

--- src/components/ShowPost.tsx
+++ src/components/ShowPost.tsx
@@ -1,7 +1,9 @@
 import parse from 'html-react-parser';
 
 const ShowPost = ({htmlContent}:{htmlContent: string})=>{
-   return <>{parse(htmlContent)}</>
+   return <div className='p-2 bg-gray-200 m-3'>
+      <div className='tiptap'>{parse(htmlContent)}</div>
+   </div>
 }
 
 export default ShowPost;
